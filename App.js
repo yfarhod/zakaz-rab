f4a57e992d2278a6fef5b7e7a577f4f761915fda
yfarhod <yfarhod75@gmail.com>
2022-09-19 20:51:16 +0200

    upd

--- App.js
+++ App.js
@@ -5,18 +5,20 @@ import { NativeWindStyleSheet } from "nativewind";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
 });
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
-      <View className="flex-1 items-center justify-center p-4 bg-lime-300">
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
