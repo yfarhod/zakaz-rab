4204ceb8c42f6b2e43192a967b1a4117f655c531
yfarhod <yfarhod75@gmail.com>
2022-09-19 20:31:37 +0200

    update

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 import { NativeWindStyleSheet } from "nativewind";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
@@ -9,9 +12,11 @@ NativeWindStyleSheet.setOutput({
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-lime-300">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View className="flex-1 items-center justify-center p-4 bg-lime-300">
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
