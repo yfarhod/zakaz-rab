9faeb9dbc4b8b7f230e0551c3c44ba0cb724b867
yfarhod <yfarhod75@gmail.com>
2022-09-21 22:28:31 +0200

    upd23

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, View, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 
@@ -13,7 +13,16 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView>
-      <Text className="text-blue-700">HomeScreen</Text>
+      <Text className="text-blue-700 mt-5">
+        <View>
+          <Image
+            source={{
+              uri: "https://cdn.britannica.com/47/7247-004-44F420D7/Flag-Uzbekistan.jpg",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+        </View>
+      </Text>
     </SafeAreaView>
   );
 };
