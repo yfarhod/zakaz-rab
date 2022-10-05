3089b93facf018363137a9b42fc7a77bc64e1779
yfarhod <yfarhod75@gmail.com>
2022-10-05 22:32:20 +0200

    upd33

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -1,6 +1,7 @@
 import { SafeAreaView, Text, View, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -12,7 +13,7 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
           source={{
@@ -20,12 +21,16 @@ const HomeScreen = () => {
           }}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
-        <View>
+        <View className="flex-1">
           <Text className="font-bold text-gray-400 text-xs">
             Заказать сейчас
           </Text>
-          <Text className="font-bold text-xl">Местоположение</Text>
+          <Text className="font-bold text-xl">
+            Местоположение
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
         </View>
+        <UserIcon size={35} color="#00CCBB" />
       </View>
     </SafeAreaView>
   );
